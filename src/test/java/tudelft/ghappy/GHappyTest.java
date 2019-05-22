283253375600f5ac9f258cfac2688a347c8cbcb1
ahmed-fatouh <39876806+ahmed-fatouh@users.noreply.github.com>
2019-05-22 05:02:19 +0200

    Add tests for GHappy class

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,44 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+
+    private GHappy ghappy;
+    private boolean result;
+
+    @BeforeEach
+    public void Initialize(){
+        this.ghappy = new GHappy();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"nudge", "google", "gig"})
+    public void SeparatedGs(String word){
+        this.result = ghappy.gHappy(word);
+        Assertions.assertFalse(this.result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"toggle", "xtuggggmk", "ggg", "gguuhhgg", "knggguuggo", "ggxggxgg"})
+    public void ConsecutiveGs(String word){
+        this.result = ghappy.gHappy(word);
+        Assertions.assertTrue(this.result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"goggle", "gxgg", "uxgggxcgh"})
+    public void MixedSeparatedAndConsecutiveGs(String word){
+        this.result = ghappy.gHappy(word);
+        Assertions.assertFalse(this.result);
+    }
+
+    @Test
+    public void CheckForNullInputs(){
+        Assertions.assertThrows(AssertionError.class, () -> ghappy.gHappy(null));
+    }
 }
