435194eb38dc68a212a7908486656eef38f02dd3
ahmed-fatouh <39876806+ahmed-fatouh@users.noreply.github.com>
2019-05-23 23:24:44 +0200

    Update GHappy Class
    This update makes the test suite to have 100% branch coverage using IntelliJ IDEA Tracing.

--- src/main/java/tudelft/ghappy/GHappy.java
+++ src/main/java/tudelft/ghappy/GHappy.java
@@ -4,10 +4,10 @@ public class GHappy {
 
     public boolean gHappy(String str) {
         assert str!=null;
-        for(int i = 0; i < str.length(); i++) {
+        for(int i = 0; i < str.length()-1; i++) {
             if(str.charAt(i) == 'g') {
                 if (i > 0 && str.charAt(i-1) == 'g') { continue; }
-                if (i+1 < str.length() && str.charAt(i+1) == 'g') { continue; }
+                if (str.charAt(i+1) == 'g') { continue; }
                 return false;
             }
         }
