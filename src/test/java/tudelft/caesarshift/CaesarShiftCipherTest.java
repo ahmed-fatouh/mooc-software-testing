a2bf713840d42f4df6a1c09ca1c910dbfd741b42
ahmed-fatouh <39876806+ahmed-fatouh@users.noreply.github.com>
2019-05-23 04:22:27 +0200

    Add tests for CaesarShiftCipher class

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,49 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class CaesarShiftCipherTest {
 
+    private CaesarShiftCipher caesar;
+
+    @BeforeEach()
+    public void Initialize(){
+        this.caesar = new CaesarShiftCipher();
+    }
+
+    @ParameterizedTest()
+    @CsvSource({"abc,3,def", "xyz,3,abc", "a o,1,b p", "f,7,m", "yyy,5,ddd"})
+    public void PositiveShiftValue(String message, int shift, String expected){
+        String result = caesar.CaesarShiftCipher(message, shift);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @ParameterizedTest()
+    @CsvSource({"abc,-3,xyz", "xyz,-3,uvw", "b p,-1,a o", "m,-7,f", "ddd,-5,yyy"})
+    public void NegativeShiftValue(String message, int shift, String expected){
+        String result = caesar.CaesarShiftCipher(message, shift);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @ParameterizedTest()
+    @CsvSource({"abc", "xyz", "t   v h" ,"s", "qqq"})
+    public void ZeroShiftValue(String message){
+        String result = caesar.CaesarShiftCipher(message, 0);
+        Assertions.assertEquals(message, result);
+    }
+
+    @ParameterizedTest()
+    @CsvSource({"q!2", "@#$%", "Car", "C^2", "2010"})
+    public void InvalidMessage(String message){
+        String result;
+        result = caesar.CaesarShiftCipher(message, -3);
+        Assertions.assertEquals("invalid", result);
+        result = caesar.CaesarShiftCipher(message, 0);
+        Assertions.assertEquals("invalid", result);
+        result = caesar.CaesarShiftCipher(message, 3);
+        Assertions.assertEquals("invalid", result);
+    }
 }
